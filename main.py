427c713d1a0882d1dbfe9d36757cbd236fc7ce54
RenanLemes <renan_ll@hotmail.com>
2023-07-27 16:37:52 -0300

    Chores:penar como seria em class

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from __future__ import print_function
+
+import os.path
+
+# import pandas as pd
+# import numpy as np
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class GoogleSheets ():
+    
+    
+        
+    def Conection (self,args):
+        pass
+
+    def Read (self, args):
+        pass
+
+    def Update (self, args):
+        pass
+
+    def Delete (self, args):
+        pass
+
+    pass
+
 print("ola")
