a34c092d0cd19ede61dd23ce7b72e4bc7dc9c6ec
RenanLemes <renan_ll@hotmail.com>
2023-08-04 20:51:51 -0300

    Chores:erros com as credenciais

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
 
+''' 
+    Importando as libs
+'''
+from fastapi import FastAPI
 # import pandas as pd
 # import numpy as np
 
@@ -9,17 +12,45 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import json
+import os
 
-def Conection ():
-    pass
-
+## ------------------------------------------------------- ## 
 app = FastAPI()
+
+''' 
+    Metodos da API
+'''
+
+def Conection (SCOPES = 'https://www.googleapis.com/auth/spreadsheets'):
+    creds = None
+    
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    if os.path.exists('C:\\Users\\RenanLemes\\Desktop\\Projeto_\\ApiSheet\\token.json'):
+        creds = Credentials.from_authorized_user_file('C:\\Users\\RenanLemes\\Desktop\\Projeto_\\ApiSheet\\token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
 
 ## interesante o fastapi 
 @app.post('/SheetsPull') 
 def SheetsPull (data:dict):
-    
-    return {'reseive':data}
+    cred = Conection()
+
+    return {"value":'ok'}
 
 
 ## Deixar a class para depois fazer o mais cru primeiro
