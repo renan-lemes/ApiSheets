74c582c5ad8e2ae0593a8b903e812aa8f67b81c3
RenanLemes <renan_ll@hotmail.com>
2023-08-04 20:08:32 -0300

    Chores:Iniciando a api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from fastapi import FastAPI
 
 # import pandas as pd
 # import numpy as np
@@ -8,21 +8,35 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import json
 
-class GoogleSheets:
-    
-    def Conection (self,args):
-        pass
-
-    def Read (self, args):
-        pass
-
-    def Update (self, args):
-        pass
-
-    def Delete (self, args):
-        pass
-
+def Conection ():
     pass
 
-print("ola")
+app = FastAPI()
+
+## interesante o fastapi 
+@app.post('/SheetsPull') 
+def SheetsPull (data:dict):
+    
+    return {'reseive':data}
+
+
+## Deixar a class para depois fazer o mais cru primeiro
+# class GoogleSheets:
+    
+#     def Conection (self,args):
+#         pass
+
+#     def Read (self, args):
+#         pass
+
+#     def Update (self, args):
+#         pass
+
+#     def Delete (self, args):
+#         pass
+
+#     pass
+
+# print("ola")
