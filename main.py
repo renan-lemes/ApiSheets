d815fa5a047cab31ec7d7936ae6e40ad28a02b9c
RenanLemes <renan_ll@hotmail.com>
2023-08-07 00:02:10 -0300

    Chores:Arrumando deu certo as questoes das cred

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from fastapi import FastAPI
 # import pandas as pd
 # import numpy as np
 
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
-
-
-
-
-import gspread
-from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+
+
+# import gspread
+# from google.oauth2.credentials import Credentials
+# from google.auth.transport.requests import Request
 # from google.oauth2.service_account import Credentials
 
 import json
@@ -30,29 +30,56 @@ app = FastAPI()
     Metodos da API
 '''
 
-def Conection (SCOPES = 'https://www.googleapis.com/auth/spreadsheets'):
-    scope = SCOPES
+# import gspread
+# from google.oauth2.credentials import Credentials
+# from google.auth.transport.requests import Request
 
-    # Carregar as credenciais do arquivo JSON
+app = FastAPI()
+
+def Read_Sheets (SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME, creds):
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets() ## Pegou o arquivo inteiro 
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,range=SAMPLE_RANGE_NAME).execute() ## aqui que passo qual planilha quero ler e qual intervalo de celulas vou ler
+    values = result.get('values', []) ## esse cara que diz se quero pegar os valores ou estilização do google
+
+    return values
+
+def Conection (SCOPES = ['https://www.googleapis.com/auth/spreadsheets']):
     creds = None
-    if creds and creds.valid:
-        creds.refresh(Request())
-    else:
-        creds = Credentials.from_authorized_user_info(
-            "token.json", scopes=scope
-        )
+    
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
 
-    client = gspread.authorize(creds)
-    return client
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    
+    return creds
 
 
+@app.get("/read_google_sheet/")
+async def read_google_sheet(sheet_id: str, range_plan:str):
 
-## interesante o fastapi 
-@app.post('/SheetsPull') 
-async def SheetsPull (data:dict):
-    cred = Conection()
+    
+    # Conectar ao Google Sheets
+    client = Conection()
+    # print(client)
+    data = Read_Sheets(sheet_id,range_plan, client)
 
-    return {"value":'ok'}
+    return {"data": data}
+
+    
 
 
 ## Deixar a class para depois fazer o mais cru primeiro
